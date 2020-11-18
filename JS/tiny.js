9210991f1cc0cfea6443599d7659bd7ac4526a63
Pete OConnell <manchesteroc@gmail.com>
2020-11-18 16:57:53 +0000

    website

--- JS/tiny.js
+++ JS/tiny.js
@@ -55,15 +55,7 @@ tinymce.init({
     },
     content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
     save_onsavecallback: function () { 
-        $.ajax({   
-            type: "POST",
-            data : $("textarea#full-featured-non-premium").serialize(),
-            cache: false,  
-            url: "process.php",   
-            success: function(data){
-                $("#results").html(data);                       
-            }   
-        }); 
+        
         console.log('Saved'); 
     }
   });
