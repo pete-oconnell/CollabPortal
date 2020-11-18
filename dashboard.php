699e10afaa37659ef61b8ff73237a7a5b01ab5d3
Pete OConnell <manchesteroc@gmail.com>
2020-11-18 14:12:44 +0000

    website

--- dashboard.php
+++ dashboard.php
@@ -4,10 +4,12 @@ include_once("template/header.php");
 
 <body>
 <div class="grid-container">
-  <div class="header"><div class="float-left margin-5">left content</div><div class="float-right margin-10">right content</div></div>
-  <div class="left-content">this is the left content</div>
-  <div class="right-content">this is the right content</div>
-  <div class="content">this is the main content</div>
+    <div class="header"><div class="float-left margin-5">left content</div><div class="float-right margin-10">right content</div></div>
+    <div class="left-content">this is the left content</div>
+    <div class="right-content">this is the right content</div>
+    <div class="content">
+        <div class="breadcrumb">this > is > a > breadcrumb</div>
+    </div>
 </div>
 
 <?php
