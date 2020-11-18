39fe99bb169b9994b5c5f97744437f1d17f6b284
Pete OConnell <manchesteroc@gmail.com>
2020-11-18 14:42:33 +0000

    website

--- dashboard.php
+++ dashboard.php
@@ -26,6 +26,16 @@ include_once("template/header.php");
     </div>
     <div class="right-content">
         <h3>Details</h3>
+        Created 3 years ago by Pete<br>
+        Updated 6 months ago by Bob<br>
+        <h3>Actions</h3>
+        New Page<br>
+        Edit<br>
+        Copy<br>
+        Move<br>
+        Revisions<br>
+        Permissions<br>
+        Delete<br>
     </div>
     <div class="content">
         <div class="breadcrumb">this > is > a > breadcrumb</div>
