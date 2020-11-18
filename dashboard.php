9bb44915ae8f497a2b0d9aa97e2a20d6d00af6ba
Pete OConnell <manchesteroc@gmail.com>
2020-11-18 14:43:43 +0000

    website

--- dashboard.php
+++ dashboard.php
@@ -26,8 +26,19 @@ include_once("template/header.php");
     </div>
     <div class="right-content">
         <h3>Details</h3>
-        Created 3 years ago by Pete<br>
-        Updated 6 months ago by Bob<br>
+        <svg class="svg-icon" data-icon="history" role="presentation" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M13 3c-4.97 0-9 4.03-9 9H1l3.89 3.89.07.14L9 12H6c0-3.87 3.13-7 7-7s7 3.13 7 7-3.13 7-7 7c-1.93 0-3.68-.79-4.94-2.06l-1.42 1.42C8.27 19.99 10.51 21 13 21c4.97 0 9-4.03 9-9s-4.03-9-9-9zm-1 5v5l4.28 2.54.72-1.21-3.5-2.08V8H12z"></path>
+</svg>Revision #5<br>
+<svg class="svg-icon" data-icon="star" role="presentation" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+    <path d="M0 0h24v24H0z" fill="none"></path>
+</svg>Created 3 years ago by Pete<br>
+<svg class="svg-icon" data-icon="edit" role="presentation" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path>
+    <path d="M0 0h24v24H0z" fill="none"></path>
+</svg>Updated 6 months ago by Bob<br>
         <h3>Actions</h3>
         New Page<br>
         Edit<br>
