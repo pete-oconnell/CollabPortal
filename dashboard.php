80b34ba60e5ee26fde0ed78b44e8aa8011c2c52b
Pete OConnell <manchesteroc@gmail.com>
2020-11-18 14:02:34 +0000

    website

--- dashboard.php
+++ dashboard.php
@@ -3,8 +3,12 @@ include_once("template/header.php");
 ?>
 
 <body>
-<div id="container"></div>
-
+<div class="grid-container">
+  <div class="header">this is the header</div>
+  <div class="left-content">this is the left content</div>
+  <div class="right-content">this is the right content</div>
+  <div class="content">this is the main content</div>
+</div>
 
 <?php
 include_once("template/footer.php");
