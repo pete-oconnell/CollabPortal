26f56958f3bd09869d3076d1d5c1fe84d27992e0
Pete OConnell <manchesteroc@gmail.com>
2020-11-18 22:07:06 +0000

    website

--- JS/tiny.js
+++ JS/tiny.js
@@ -79,7 +79,7 @@ tinymce.init({
           onAction: function() {
             var mycommentID = $.ajax({ url: "comment.php", type: 'get', dataType: 'html', cache: false, success: function(data) { result = data; console.log(result); return result; } });
             var comment = prompt('Comment with?');
-            console.log(mycommentID);
+            console.log(mycommentID.responseText);
             //editor.annotator.annotate('alpha', {
             //  uid: mycommentID,
             //  comment: comment
