86bcde88b2bacb4fd5eb03ec07d1d324a7eb0d20
Pete OConnell <manchesteroc@gmail.com>
2020-11-19 15:13:36 +0000

    website

---
+++ content.php
@@ -0,0 +1,84 @@
+<?php
+include_once("template/header.php");
+?>
+
+<body>
+<div class="grid-container">
+    <div class="header"><div class="float-left margin-5"><img src="img/icon.png"> <h1>Collaboration Portal</h1></div><div class="float-right margin-10">right content</div></div>
+    <div class="left-content">
+        <div class="recent">
+        <h3>Recent Activity</h3>
+            <ul>
+                <li>[av] [user] [activity] [page]</li>
+                <li>[av] [user] [activity] [page]</li>
+                <li>[av] [user] [activity] [page]</li>
+                <li>[av] [user] [activity] [page]</li>
+                <li>[av] [user] [activity] [page]</li>
+                <li>[av] [user] [activity] [page]</li>
+                <li>[av] [user] [activity] [page]</li>
+                <li>[av] [user] [activity] [page]</li>
+                <li>[av] [user] [activity] [page]</li>
+                <li>[av] [user] [activity] [page]</li>
+                <li>[av] [user] [activity] [page]</li>
+                <li>[av] [user] [activity] [page]</li>
+                <li>[av] [user] [activity] [page]</li>
+            </ul>
+        </div>
+    </div>
+    <div class="right-content">
+        <div class="recent">
+        <h3>Details</h3>
+        <svg class="svg-icon" data-icon="history" role="presentation" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M13 3c-4.97 0-9 4.03-9 9H1l3.89 3.89.07.14L9 12H6c0-3.87 3.13-7 7-7s7 3.13 7 7-3.13 7-7 7c-1.93 0-3.68-.79-4.94-2.06l-1.42 1.42C8.27 19.99 10.51 21 13 21c4.97 0 9-4.03 9-9s-4.03-9-9-9zm-1 5v5l4.28 2.54.72-1.21-3.5-2.08V8H12z"></path>
+</svg>Revision #5<br>
+<svg class="svg-icon" data-icon="star" role="presentation" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"></path>
+    <path d="M0 0h24v24H0z" fill="none"></path>
+</svg>Created 3 years ago by Pete<br>
+<svg class="svg-icon" data-icon="edit" role="presentation" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path>
+    <path d="M0 0h24v24H0z" fill="none"></path>
+</svg>Updated 6 months ago by Bob<br>
+        <h3>Actions</h3>
+        <svg class="svg-icon" data-icon="edit" role="presentation" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path>
+    <path d="M0 0h24v24H0z" fill="none"></path>
+</svg>Edit<br>
+<svg class="svg-icon" data-icon="copy" role="presentation" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"></path>
+</svg>Copy<br>
+<svg class="svg-icon" data-icon="folder" role="presentation" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z"></path>
+    <path d="M0 0h24v24H0z" fill="none"></path>
+</svg>Move<br>
+<svg class="svg-icon" data-icon="history" role="presentation" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M13 3c-4.97 0-9 4.03-9 9H1l3.89 3.89.07.14L9 12H6c0-3.87 3.13-7 7-7s7 3.13 7 7-3.13 7-7 7c-1.93 0-3.68-.79-4.94-2.06l-1.42 1.42C8.27 19.99 10.51 21 13 21c4.97 0 9-4.03 9-9s-4.03-9-9-9zm-1 5v5l4.28 2.54.72-1.21-3.5-2.08V8H12z"></path>
+</svg>Revisions<br>
+<svg class="svg-icon" data-icon="lock" role="presentation" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <defs>
+        <path d="M0 0h24v24H0V0z" id="a"></path>
+    </defs>
+    <clipPath id="b">
+        <use overflow="visible" xlink:href="#a"></use>
+    </clipPath>
+    <path clip-path="url(#b)" d="M12 17c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm6-9h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zM8.9 6c0-1.71 1.39-3.1 3.1-3.1s3.1 1.39 3.1 3.1v2H8.9V6zM18 20H6V10h12v10z"></path>
+</svg>Permissions<br>
+<svg class="svg-icon" data-icon="delete" role="presentation" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path>
+    <path d="M0 0h24v24H0z" fill="none"></path>
+</svg>Delete<br>
+    </div>
+    </div>
+    <div class="content">
+        <div class="nfi">
+            <h3>Content Title</h3>
+            This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content This is some content 
+        </div>
+    </div>
+</div>
+
+<?php
+include_once("template/footer.php");
