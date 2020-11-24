8d057ea94f06f05dce6313663f9680ab5fea82a8
Pete OConnell <manchesteroc@gmail.com>
2020-11-24 08:14:03 +0000

    Login view changes

--- app/Views/login.php
+++ app/Views/login.php
@@ -1,5 +1,5 @@
 <div class="grid-container">
 <div class="left-content">left content</div>
-<div class="right-content">right content</div>
 <div class="content">main content</div>
+<div class="right-content">right content</div>
 </div>
