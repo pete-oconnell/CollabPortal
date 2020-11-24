8ecc3a5063274b1cad90cc0902086d43057adbb8
Pete OConnell <manchesteroc@gmail.com>
2020-11-24 05:55:21 +0000

    Login view changes

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -6,6 +6,7 @@ class Login extends Controller
 {
 	public function index()
 	{
+        $data = array();
         echo view('template/header', $data);
         echo view('login', $data);
         echo view('template/footer', $data);
