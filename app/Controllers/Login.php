a9fed4c06b961e49b33b2c126ca4e5d5159712be
Pete OConnell <manchesteroc@gmail.com>
2020-11-24 06:07:13 +0000

    Login view changes

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -7,6 +7,7 @@ class Login extends Controller
 	public function index()
 	{
         $data = array();
+        $data['title'] = "login";
         echo view('template/header', $data);
         echo view('login', $data);
         echo view('template/footer', $data);
---
+++ app/Views/template/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Collaboration Portal / <?=esc($title);?></title>
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+
+</head>
+<body>
+    <header>
+        <div class="nav">this is the nav bar</div>
+    </header>
