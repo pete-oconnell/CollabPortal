db230c19225e6357cda4764a9b427d11347241f1
Pete OConnell <manchesteroc@gmail.com>
2020-11-24 08:15:27 +0000

    Login view changes

--- app/Views/login.php
+++ app/Views/login.php
@@ -1,5 +1,5 @@
 <div class="grid-container">
 <div class="left-content">left content</div>
-<div class="content">main content</div>
+<div class="content">main content which should be long long long long long but for some reaosn its not</div>
 <div class="right-content">right content</div>
 </div>
