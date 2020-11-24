a74528c6e5f919a98bd47a5844352c9a846cd1de
Pete OConnell <manchesteroc@gmail.com>
2020-11-24 05:48:25 +0000

    Login view changes

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -4,7 +4,15 @@ class Login extends BaseController
 {
 	public function index()
 	{
-		return view('login');
+        if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
+        {
+                // Whoops, we don't have a page for that!
+                show_404();
+        }
+
+        $this->load->view('template/header', $data);
+        $this->load->view('login', $data);
+        $this->load->view('template/footer', $data);
 	}
 
 	//--------------------------------------------------------------------
